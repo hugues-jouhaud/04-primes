3e45881137945e2f953bd8d847f8a8fd91ff93d8
hugues-jouhaud <hugues.jouhaud@edu.esiee.fr>
2025-09-15 15:03:48 +0200

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from math import sqrt
+import numpy as np
 
 #### Fonction secondaire
 
 
 def isprime(p):
 
-    # votre code ici
+    Max = 100
+    for k in range(2, Max+1):
+      nbrChoisi = k
+      diviseursPotentiels =  int(np.ceil(np.sqrt(nbrChoisi)))
+      nbrPremier=True
+      for i in range(2,diviseursPotentiels+1):
+        if nbrChoisi % i == 0:
+          nbrPremier=False
+          break
+      if nbrPremier:
+        print(nbrChoisi)
+    
 
     pass
 
